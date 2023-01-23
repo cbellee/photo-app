a1dc49e42a0c267cb4c2cbf0511394edc04da41b
Chris Bellee <8634922+cbellee@users.noreply.github.com>
2023-01-23 13:50:49 +1100

    added tag logging

---
+++ scripts/scale.ps1
@@ -0,0 +1,46 @@
+param (
+    $storageAccountName = 'storaodiwusgqeyiu',
+    $containerName = 'uploads',
+    $localPath = '../images',
+    $rgName = 'photo-app-test-rg',
+    $containers = @('uploads','thumbs','images')
+)
+
+# Install-Module -Name Az.App
+# Register-AzResourceProvider -ProviderNamespace Microsoft.App
+
+# remove existing blobs
+$ctx = (Get-AzStorageAccount -ResourceGroupName $rgName -Name $storageAccountName).Context 
+$containers | ForEach-Object {
+    Get-AzStorageBlob -Container $_ -Context $ctx | Remove-AzStorageBlob 
+}
+
+# list # of replicas
+"# of Replicas: $(Get-AzContainerAppRevision -ContainerAppName resize -ResourceGroupName $rgName | 
+    Where-Object Active -eq $true | 
+        Select-Object -ExpandProperty replica)"
+
+Get-ChildItem $localPath -Directory | ForEach-Object {
+    $collection = $_.Name
+    Get-ChildItem $_.FullName -Directory | ForEach-Object {
+        $album = $_.Name
+        Get-ChildItem $_.FullName -File | ForEach-Object {
+        Set-AzStorageBlobContent `
+            -File $($_.FullName) `
+            -Container $containerName `
+            -Blob "$collection/$album/$($_.Name)" `
+            -Metadata @{Collection=$collection;Album=$album;Name=$($_.Name)} `
+            -Tag @{Collection=$collection;Album=$album} `
+            -Context $ctx `
+            -Force -Verbose
+        }
+<#         azcopy copy "$($_.FullName)/*" "https://$storageAccountName.blob.core.windows.net/$containerName/$collection/$album" `
+        --blob-tags="Collection=$collection&Album=$album" `
+        --metadata "Collection=$collection;Album=$album;Name=$($_.Name)" #>
+    }
+}
+
+# list # of replicas
+"# of Replicas: $(Get-AzContainerAppRevision -ContainerAppName resize -ResourceGroupName $rgName | 
+    Where-Object Active -eq $true | 
+        Select-Object -ExpandProperty replica)"
